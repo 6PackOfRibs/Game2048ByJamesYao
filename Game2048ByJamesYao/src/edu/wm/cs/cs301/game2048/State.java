6bb9271b4b7c133d4d0cd2ae0d81887148807062
JamesYaoWM <jtyao@email.wm.edu>
2020-09-06 21:24:53 -0400

    Now passes testGetValue, testSetEmptyBoard, and testAllMovesOnEmptyBoard

--- Game2048ByJamesYao/src/edu/wm/cs/cs301/game2048/State.java
+++ Game2048ByJamesYao/src/edu/wm/cs/cs301/game2048/State.java
@@ -2,30 +2,35 @@ package edu.wm.cs.cs301.game2048;
 
 public class State implements GameState {
 
+	int boardPositions[] = new int [16];
 	public State(State currentState) {
-		// TODO Auto-generated constructor stub
+		// new State inherits currentState's board positions
+		boardPositions = currentState.boardPositions;
 	}
 	
 	public State() {
-		// TODO Auto-generated constructor stub
+		// contains the values at the positions
 	}
 
 	@Override
 	public int getValue(int x, int y) {
-		// TODO Auto-generated method stub
-		return 0;
+		// returns value at (x,y) position
+		return boardPositions[x + (4 * y)];
 	}
 
 	@Override
 	public void setValue(int x, int y, int value) {
 		// TODO Auto-generated method stub
-
+		boardPositions[x + (4 * y)] = value;
+		
 	}
 
 	@Override
 	public void setEmptyBoard() {
 		// TODO Auto-generated method stub
-
+		for (int i = 0; i < boardPositions.length; i++) {
+			boardPositions[i] = 0;
+		}
 	}
 
 	@Override
